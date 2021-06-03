f4d4089c6d1e12138fdd986c5cb950fa93522aaf
Lorenzo Catalano <catalanolorenzo95@gmail.com>
2021-06-03 16:07:27 +0200

    variante senza if

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
+// for (var i = 1; i <= 100; i++) {
+
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log(i + " FizzBuzz");
+//     } else if (i % 3 == 0) {
+//         console.log(i + " Fizz");
+//     } else if (i % 5 == 0) {
+//         console.log(i + " Buzz");
+//     } else{
+//         console.log(i);
+//     }
+// }
+
+
+var numero = "";
+
 for (var i = 1; i <= 100; i++) {
 
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log(i + " FizzBuzz");
-    } else if (i % 3 == 0) {
-        console.log(i + " Fizz");
-    } else if (i % 5 == 0) {
-        console.log(i + " Buzz");
-    } else{
-        console.log(i);
+    numero = i;
+
+    if (i % 3 == 0) {
+        numero = numero + "Fizz";
     }
+    
+    if (i % 5 == 0) {
+        numero = numero + "Buzz";
+    }
+
+    console.log(numero);
 }
